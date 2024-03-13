7f086169a6ebcb2b1e4def56df65d52c3dcbb7ab
Rahul Singh <158085255+Fibonaccighost@users.noreply.github.com>
2024-03-13 20:16:12 +0530

    Add files via upload

---
+++ scriptpaper.js
@@ -0,0 +1,53 @@
+
+ // Function to store previous page URL in localStorage
+ function storePreviousPage() {
+    localStorage.setItem('previousPage', window.location.href);
+}
+
+// Check if the game has been played and redirect accordingly
+window.onload = function() {
+    if (localStorage.getItem('gamePlayed')) {
+        let previousPage = localStorage.getItem('previousPage');
+        if (previousPage) {
+            localStorage.removeItem('gamePlayed'); // Remove the gamePlayed flag
+            localStorage.removeItem('previousPage'); // Remove the previous page URL
+            window.location.href = "game.html"; // Redirect to the previous page
+        }
+    }
+};
+
+let trigger = document.getElementById("trigger2")
+
+function compChoice(rNumber) {
+    switch (rNumber) {
+        case 0:
+            document.getElementById("container").insertAdjacentHTML("beforeend",`<div><p>Bot</p><img src="stone.png"></div>`)
+            break;
+        case 1:
+            document.getElementById("container").insertAdjacentHTML("beforeend",`<div><p>Bot</p><img src="paper.png"></div>`)
+            break;
+        case 2:
+            document.getElementById("container").insertAdjacentHTML("beforeend",`<div><p>Bot</p><img src="Scissor.png"></div>`)
+            break;
+    
+        default:
+            break;
+    }
+    trigger.remove()
+    // Set gamePlayed flag indicating the game has been played
+    localStorage.setItem('gamePlayed', true);
+    // Store the URL of the current page as the previous page
+    storePreviousPage();
+}
+
+trigger.addEventListener("click", () => {
+    let rNumber = Math.floor(Math.random() * 3);
+    compChoice(rNumber);
+    if (rNumber == 0) {
+        document.getElementById("result").insertAdjacentHTML("beforeend", `<p><strong>WIN</strong></p>`);
+    } else if (rNumber == 1) {
+        document.getElementById("result").insertAdjacentHTML("beforeend", `<p><strong>TIE</strong></p>`);
+    } else {
+        document.getElementById("result").insertAdjacentHTML("beforeend", `<p><strong>LOSE</strong></p>`);
+    }
+});
